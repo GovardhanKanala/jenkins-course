e55447f0d3c4420168b3d6440be876c7fb4361d3
GovardhanKanala <govardhankanala@gmail.com>
2019-09-17 20:32:17 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('govardhankanala/gova')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('dockerhub1')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
